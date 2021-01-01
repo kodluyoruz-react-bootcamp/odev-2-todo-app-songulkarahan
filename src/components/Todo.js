5e248eebb3d192dc5eb1ae48de638f1946cbe606
songulkarahan <songul.karahan@bil.omu.edu.tr>
2021-01-01 14:29:13 +0300

    added toggleTodoCompleted function

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,16 +1,31 @@
 import React from "react";
 
-function Todo({ text, id, todos, setTodos }) {
+function Todo({ text, id, todo, todos, setTodos }) {
   const deleteTodo = (id) => {
     const filtered = todos.filter((todo) => todo.id !== id);
     setTodos(filtered);
+  };
+
+  const toggleTodoCompleted = () => {
+    setTodos(
+      todos.map((item) => {
+        if (item.id === todo.id) {
+          return { ...item, completed: !item.completed };
+        }
+        return item;
+      })
+    );
   };
 
   return (
     <div>
       <li>
         <div className="view">
-          <input className="toggle" type="checkbox" />
+          <input
+            onClick={toggleTodoCompleted}
+            className="toggle"
+            type="checkbox"
+          />
           <label>{text}</label>
           <button onClick={() => deleteTodo(id)} className="destroy" />
         </div>
